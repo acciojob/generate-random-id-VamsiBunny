3447cf507049970ffd2d3a46529062189a9a8b02
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-04 20:18:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function makeid(l) {
-     let result=" "
-     let char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmanoqrsuvwxyz1234567890"
-	 for(let i=o;i<l;i++){
-		 result+=char_list.charAt(
-			 Math.floor(
-				 Math.random*char_list.length;
-			 )
-		 )
-		 return result;
-	 }
-	
+    let result = ""; 
+    let char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    
+    for (let i = 0; i < l; i++) {
+        result += char_list.charAt(
+            Math.floor(Math.random() * char_list.length) 
+        );
+    }
+    
+    return result; 
 }
 
 //  Do not change the code below.
- const l = prompt("Enter a number.");
- alert(makeid(l)); 
+const l = prompt("Enter a number.");
+alert(makeid(l)); 
